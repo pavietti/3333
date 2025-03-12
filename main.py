d2fe9a27fe8f394e8148850e7d709e47f39f00f9
pavietti <pavel.bryzgunov@gmail.com>
2025-03-12 20:12:22 +0300

    fffff

---
+++ main.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    print("starting")
+    data_file = open("data.csv")
+    data=[]
+    for line in data_file.readlines():
+        line_parts = line.split(",")
+        index = int(line_parts[0])
+        value = float(line_parts[1])
+        text = str(line_parts[2].strip())
+        data.append([index, value, text])
+    print(data)
