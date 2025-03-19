caa4bca1709542800037bf0e26cdf41259bce2b7
pavietti <pavel.bryzgunov@gmail.com>
2025-03-19 19:30:31 +0300

    45

---
+++ main2.py
@@ -0,0 +1,11 @@
+from switch import Switch
+
+
+if __name__ == "__main2__":
+    sw1 = Switch()
+    sw1.name = "Q1"
+    sw1.position = 1
+    print(sw1)   
+    sw2=Switch()
+    print(sw2)
+    
---
+++ switch.py
@@ -0,0 +1,6 @@
+class Switch:
+    switch_count = 0
+    
+    def __init__(self):
+        self.name = ""
+        self.position = 0
