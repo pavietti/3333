236f7735a25d46a2263cca90e74f587843b1362f
pavietti <pavel.bryzgunov@gmail.com>
2025-03-19 19:42:50 +0300

    er

--- main2.py
+++ main2.py
@@ -3,9 +3,12 @@ from switch import Switch
 
 if __name__ == "__main2__":
     sw1 = Switch()
+    Switch.switch_count = 3
     sw1.name = "Q1"
     sw1.position = 1
+    sw1.switch_count = 5
     print(sw1)   
     sw2=Switch()
     print(sw2)
-    
+    sw3=sw2
+    pass
--- switch.py
+++ switch.py
@@ -1,6 +1,6 @@
 class Switch:
     switch_count = 0
-    
+    switch_count +=1
     def __init__(self):
         self.name = ""
         self.position = 0
